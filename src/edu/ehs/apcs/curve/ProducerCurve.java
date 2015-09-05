0f3e6fa28dba3fa77d79fac2f07d0229d96fd460
Gabriel Rivera <elggrivera@gmail.com>
2015-09-05 16:16:50 -0500

    Added constructors to the ProducerCurve class

--- src/edu/ehs/apcs/curve/ProducerCurve.java
+++ src/edu/ehs/apcs/curve/ProducerCurve.java
@@ -1,6 +1,29 @@
 package edu.ehs.apcs.curve;
 
-public class ProducerCurve {
+import edu.ehs.apcs.point.Point;
 
+public class ProducerCurve 
+{
+	private Point[] curvePoints;
+	
+	/**
+	 * Constructor method
+	 * Instantiated curvePoints to an array of 10 Points
+	 */
+	public ProducerCurve()
+	{
+		super();
+		this.curvePoints = new Point[10];
+	}
+	
+	/**
+	 * Instantiates the curvePoints[] for the ProducerCurve Class
+	 * from a Point[]
+	 * @param curvePoints
+	 */
+	public ProducerCurve(Point[] curvePoints) 
+	{
+		super();
+		this.curvePoints = curvePoints;
+	}
 }
- 
