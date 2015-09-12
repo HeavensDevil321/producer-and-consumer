d51b2c10f3d80bba2d2f6947283b7cc2a3fae404
Gabriel Rivera <elggrivera@gmail.com>
2015-09-11 19:33:56 -0500

    test for ProducerCurve

---
+++ src/edu/ehs/apcs/curve/ProducerCurveTest.java
@@ -0,0 +1,120 @@
+/**
+ * 
+ */
+package edu.ehs.apcs.curve;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Gabriel
+ *
+ */
+public class ProducerCurveTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link edu.ehs.apcs.curve.ProducerCurve#ProducerCurve()}.
+	 */
+	@Test
+	public final void testProducerCurve() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.ehs.apcs.curve.ProducerCurve#ProducerCurve(edu.ehs.apcs.point.Point[])}.
+	 */
+	@Test
+	public final void testProducerCurvePointArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.ehs.apcs.curve.ProducerCurve#addPointToCurve(edu.ehs.apcs.point.Point, int)}.
+	 */
+	@Test
+	public final void testAddPointToCurve() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.ehs.apcs.curve.ProducerCurve#copyCurve(edu.ehs.apcs.point.Point[])}.
+	 */
+	@Test
+	public final void testCopyCurve() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.ehs.apcs.curve.ProducerCurve#deletePointAtBeginningOfCurve(edu.ehs.apcs.point.Point)}.
+	 */
+	@Test
+	public final void testDeletePointAtBeginningOfCurve() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.ehs.apcs.curve.ProducerCurve#deletePointAtMiddleOfCurve(int)}.
+	 */
+	@Test
+	public final void testDeletePointAtMiddleOfCurve() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.ehs.apcs.curve.ProducerCurve#deletePointAtEndOfCurve(edu.ehs.apcs.point.Point)}.
+	 */
+	@Test
+	public final void testDeletePointAtEndOfCurve() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.ehs.apcs.curve.ProducerCurve#setPoint(edu.ehs.apcs.point.Point, int)}.
+	 */
+	@Test
+	public final void testSetPoint() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link edu.ehs.apcs.curve.ProducerCurve#isEmpty()}.
+	 */
+	@Test
+	public final void testIsEmpty() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
