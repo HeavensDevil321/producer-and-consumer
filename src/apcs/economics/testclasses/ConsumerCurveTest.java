bfcc9e2a47e464a737a85e704b2313a6219f6055
Gabriel Rivera <elggrivera@gmail.com>
2015-09-24 16:41:53 -0500

    small fixes

--- src/apcs/economics/testclasses/ConsumerCurveTest.java
+++ src/apcs/economics/testclasses/ConsumerCurveTest.java
@@ -5,9 +5,6 @@ package apcs.economics.testclasses;
 
 import java.util.ArrayList;
 
-import apcs.economics.ConsumerCurve;
-import apcs.economics.Point;
-
 /**
  * @author German G. Rivera
  * @date 8/26/2015
@@ -16,60 +13,60 @@ import apcs.economics.Point;
  */
 public class ConsumerCurveTest 
 {
-		public static void main(String[] args) 
-		{
-			ConsumerCurve test = new ConsumerCurve();
-			
-			System.out.println("Size:" + test.size());
-			System.out.println("List:" + test.toString());
-			
-			ArrayList<Point> curvePoints = new ArrayList<Point>(); 			
-		
-			curvePoints.add(new Point(0, 0.0));
-			curvePoints.add(new Point(1, 1.0));
-			curvePoints.add(new Point(2, 2.0));
-			curvePoints.add(new Point(3, 3.0));
-			curvePoints.add(new Point(4, 4.0));
-			curvePoints.add(new Point(5, 5.0));
-			curvePoints.add(new Point(5, 5.0));
-			curvePoints.add(new Point(6, 6.0));
-			curvePoints.add(new Point(7, 7.0));
-			curvePoints.add(new Point(8, 7.0));			
-			curvePoints.add(new Point(4, 5.0));
-			
-			ConsumerCurve test2 = new ConsumerCurve(curvePoints);
-			System.out.println("SIZE & TOSTRING");
-			System.out.println("Size:" + test2.size());
-			System.out.println("List:" + test2.toString());
-			
-			
-			System.out.println("ADD");
-
-			test2.add(new Point(1, 3.2),  1);
-			System.out.println("List:" + test2.toString());
-			System.out.println("Size:" + test2.size());
-			
-			System.out.println("FIND");			
-			System.out.println("search point (1,3.2):" + test2.contain(new Point(1, 3.2)));
-			System.out.println("List:" + test2.toString());
-			
-			System.out.println("DELETE");
-			test2.remove(1);
-
-			test2.addPointInCurve(new Point(1, 3.2),  1);
-			System.out.println("List:" + test2.toString());
-			System.out.println("Size:" + test2.size());
-			
-			System.out.println("FIND");			
-			System.out.println("search point (1,3.2):" + test2.contain(new Point(1, 3.2)));
-			System.out.println("List:" + test2.toString());
-			
-			System.out.println("DELETE");
-			test2.deletePointInCurve(1);
-			System.out.println("Size:" + test2.size());
-			System.out.println("search point (1,3.2):" + test2.contain(new Point(1, 3.2)));
-			System.out.println("List:" + test2.toString());
-						
-			System.out.println("are the the point equal:" + test.contain(new Point(2,2.3)));	
-		}
+//		public static void main(String[] args) 
+//		{
+//			ConsumerCurve test = new ConsumerCurve();
+//			
+//			System.out.println("Size:" + test.size());
+//			System.out.println("List:" + test.toString());
+//			
+//			ArrayList<Point> curvePoints = new ArrayList<Point>(); 			
+//		
+//			curvePoints.add(new Point(0, 0.0));
+//			curvePoints.add(new Point(1, 1.0));
+//			curvePoints.add(new Point(2, 2.0));
+//			curvePoints.add(new Point(3, 3.0));
+//			curvePoints.add(new Point(4, 4.0));
+//			curvePoints.add(new Point(5, 5.0));
+//			curvePoints.add(new Point(5, 5.0));
+//			curvePoints.add(new Point(6, 6.0));
+//			curvePoints.add(new Point(7, 7.0));
+//			curvePoints.add(new Point(8, 7.0));			
+//			curvePoints.add(new Point(4, 5.0));
+//			
+//			ConsumerCurve test2 = new ConsumerCurve(curvePoints);
+//			System.out.println("SIZE & TOSTRING");
+//			System.out.println("Size:" + test2.size());
+//			System.out.println("List:" + test2.toString());
+//			
+//			
+//			System.out.println("ADD");
+//
+//			test2.add(new Point(1, 3.2),  1);
+//			System.out.println("List:" + test2.toString());
+//			System.out.println("Size:" + test2.size());
+//			
+//			System.out.println("FIND");			
+//			System.out.println("search point (1,3.2):" + test2.contain(new Point(1, 3.2)));
+//			System.out.println("List:" + test2.toString());
+//			
+//			System.out.println("DELETE");
+//			test2.remove(1);
+//
+//			test2.addPointInCurve(new Point(1, 3.2),  1);
+//			System.out.println("List:" + test2.toString());
+//			System.out.println("Size:" + test2.size());
+//			
+//			System.out.println("FIND");			
+//			System.out.println("search point (1,3.2):" + test2.contain(new Point(1, 3.2)));
+//			System.out.println("List:" + test2.toString());
+//			
+//			System.out.println("DELETE");
+//			test2.deletePointInCurve(1);
+//			System.out.println("Size:" + test2.size());
+//			System.out.println("search point (1,3.2):" + test2.contain(new Point(1, 3.2)));
+//			System.out.println("List:" + test2.toString());
+//						
+//			System.out.println("are the the point equal:" + test.contain(new Point(2,2.3)));	
+//		}
 }
--- src/apcs/economics/testclasses/ProducerCurveTest.java
+++ src/apcs/economics/testclasses/ProducerCurveTest.java
@@ -23,188 +23,126 @@ import org.junit.Test;
  * @hour 3rd hr.
  * Test for ProducerCurve
  */
-
-/**
-	 * Test method for {@link apcs.economics.ProducerCurve#ProducerCurve()}.
-	 */
-
-/**
-	 * Test method for {@link apcs.economics.ProducerCurve#ProducerCurve(apcs.economics.Point[])}.
-	 */
-
-// FIXME: potentially need to check the curvePoints variable is non-zero in ProdCurve
-
-/**
-	 * Test method for {@link apcs.economics.ProducerCurve#addPointToCurve(apcs.economics.Point, int)}.
-	 */
-
-//assertTrue("Added a point.", testProducerCurve.addPointToCurve(p1, 0));
-// TODO
-
-/**
-	 * Test method for {@link apcs.economics.ProducerCurve#copyCurve(apcs.economics.Point[])}.
-	 */
-
-/**
-	 * Test method for {@link apcs.economics.ProducerCurve#deletePointAtBeginningOfCurve(apcs.economics.Point)}.
-	 */
-
-// TODO
-
-/**
-	 * Test method for {@link apcs.economics.ProducerCurve#deletePointAtMiddleOfCurve(int)}.
-	 */
-
-// TODO
-
-/**
-	 * Test method for {@link apcs.economics.ProducerCurve#deletePointAtEndOfCurve(apcs.economics.Point)}.
-	 */
-
-// TODO
-
-/**
-	 * Test method for {@link apcs.economics.ProducerCurve#setPoint(apcs.economics.Point, int)}.
-	 */
-
-//assertTrue(testProducerCurve.getPoint(7).equals(testPoint));
-
-/**
-	 * Test method for {@link apcs.economics.ProducerCurve#isEmpty()}.
-	 */
-
-//assertTrue(testProducerCurve.toString());
-import apcs.economics.Point;
-import apcs.economics.Point;
-import apcs.economics.ProducerCurve;
-import apcs.economics.ProducerCurve;
-
-/**
- * @author German G. Rivera
- * @date 8/26/2015
- * @hour 3rd hr.
- * Test for ProducerCurve
- */
 public class ProducerCurveTest 
 {
-	Point p1 = new Point(4,5);
-	Point p2 = new Point(2,2);
-
-	/**
-	 * Test method for {@link apcs.economics.ProducerCurve#ProducerCurve()}.
-	 */
-	@Test
-	public final void testProducerCurve() 
-	{
-		ProducerCurve myProducerCurve = new ProducerCurve();
-		assertNotNull("Object Created.", myProducerCurve);
-	}
-
-	/**
-	 * Test method for {@link apcs.economics.ProducerCurve#ProducerCurve(apcs.economics.Point[])}.
-	 */
-	@Test
-	public final void testProducerCurvePointArray() 
-	{
-		Point[] curvePoints = new Point[10];
-		ProducerCurve testProducerCurve = new ProducerCurve(curvePoints);
-		assertNotNull("ProducerCurve Created.", testProducerCurve);
-		// FIXME: potentially need to check the curvePoints variable is non-zero in ProdCurve
-		assertTrue("Size of array is non-zero", testProducerCurve.isEmpty()); 
-	}
-
-	/**
-	 * Test method for {@link apcs.economics.ProducerCurve#addPointToCurve(apcs.economics.Point, int)}.
-	 */
-	@Test
-	public final void testAddPointToCurve() 
-	{
-		Point[] curvePoints = new Point[10];
-		ProducerCurve testProducerCurve = new ProducerCurve(curvePoints);
-		assertNotNull("ProducerCurve Created.", testProducerCurve);
-		
-		assertTrue("Added a point.", testProducerCurve.addPointToCurve(p1, 0));
-		// TODO
-	}
-
-	/**
-	 * Test method for {@link apcs.economics.ProducerCurve#copyCurve(apcs.economics.Point[])}.
-	 */
-	@Test
-	public final void testCopyCurve() 
-	{
-		Point[] curve1 = new Point[10];
-		ProducerCurve pc1 = new ProducerCurve(curve1);
-		Point[] curve2 = pc1.copyCurve(curve1);
-		assertArrayEquals(curve1, curve2);
-		
-		
-	}
-
-	/**
-	 * Test method for {@link apcs.economics.ProducerCurve#deletePointAtBeginningOfCurve(apcs.economics.Point)}.
-	 */
-	@Test
-	public final void testDeletePointAtBeginningOfCurve() 
-	{
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link apcs.economics.ProducerCurve#deletePointAtMiddleOfCurve(int)}.
-	 */
-	@Test
-	public final void testDeletePointAtMiddleOfCurve() 
-	{
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link apcs.economics.ProducerCurve#deletePointAtEndOfCurve(apcs.economics.Point)}.
-	 */
-	@Test
-	public final void testDeletePointAtEndOfCurve() 
-	{
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link apcs.economics.ProducerCurve#setPoint(apcs.economics.Point, int)}.
-	 */
-	@Test
-	public final void testSetPoint() 
-	{
-		Point[] pointArray = new Point[10];
-		Point testPoint = new Point(1, 1.0);
-		ProducerCurve testProducerCurve = new ProducerCurve(pointArray);
-		testProducerCurve.setPoint(testPoint, 7);
-		
-		//assertTrue(testProducerCurve.getPoint(7).equals(testPoint));
-		assertEquals(testPoint, testProducerCurve.getPointInCurve(7));
-		
-	}
-
-	/**
-	 * Test method for {@link apcs.economics.ProducerCurve#isEmpty()}.
-	 */
-	@Test
-	public final void testIsEmpty() 
-	{
-		Point[] curvePoints = new Point[10];
-		ProducerCurve testProducerCurve = new ProducerCurve(curvePoints);
-		assertTrue("Curve is Empty.", testProducerCurve.isEmpty());
-		
-		Point testPoint = new Point(1, 1.0);
-		testProducerCurve.addPointToCurve(testPoint, 5);
-		assertFalse("Curve is not empty.", testProducerCurve.isEmpty());
-	}
-
-	public final void testToString()
-	{
-		Point[] curvePoints = new Point[10];
-		ProducerCurve testProducerCurve = new ProducerCurve(curvePoints);
-		
-		
-		assertTrue(testProducerCurve.toString());
-	}
+//	Point p1 = new Point(4,5);
+//	Point p2 = new Point(2,2);
+//
+//	/**
+//	 * Test method for {@link apcs.economics.ProducerCurve#ProducerCurve()}.
+//	 */
+//	@Test
+//	public final void testProducerCurve() 
+//	{
+//		ProducerCurve myProducerCurve = new ProducerCurve();
+//		assertNotNull("Object Created.", myProducerCurve);
+//	}
+//
+//	/**
+//	 * Test method for {@link apcs.economics.ProducerCurve#ProducerCurve(apcs.economics.Point[])}.
+//	 */
+//	@Test
+//	public final void testProducerCurvePointArray() 
+//	{
+//		Point[] curvePoints = new Point[10];
+//		ProducerCurve testProducerCurve = new ProducerCurve(curvePoints);
+//		assertNotNull("ProducerCurve Created.", testProducerCurve);
+//		// FIXME: potentially need to check the curvePoints variable is non-zero in ProdCurve
+//		assertTrue("Size of array is non-zero", testProducerCurve.isEmpty()); 
+//	}
+//
+//	/**
+//	 * Test method for {@link apcs.economics.ProducerCurve#addPointToCurve(apcs.economics.Point, int)}.
+//	 */
+//	@Test
+//	public final void testAddPointToCurve() 
+//	{
+//		Point[] curvePoints = new Point[10];
+//		ProducerCurve testProducerCurve = new ProducerCurve(curvePoints);
+//		assertNotNull("ProducerCurve Created.", testProducerCurve);
+//		
+//		assertTrue("Added a point.", testProducerCurve.addPointToCurve(p1, 0));
+//		// TODO
+//	}
+//
+//	/**
+//	 * Test method for {@link apcs.economics.ProducerCurve#copyCurve(apcs.economics.Point[])}.
+//	 */
+//	@Test
+//	public final void testCopyCurve() 
+//	{
+//		Point[] curve1 = new Point[10];
+//		ProducerCurve pc1 = new ProducerCurve(curve1);
+//		Point[] curve2 = pc1.copyCurve(curve1);
+//		assertArrayEquals(curve1, curve2);
+//		
+//		
+//	}
+//
+//	/**
+//	 * Test method for {@link apcs.economics.ProducerCurve#deletePointAtBeginningOfCurve(apcs.economics.Point)}.
+//	 */
+//	@Test
+//	public final void testDeletePointAtBeginningOfCurve() 
+//	{
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	/**
+//	 * Test method for {@link apcs.economics.ProducerCurve#deletePointAtMiddleOfCurve(int)}.
+//	 */
+//	@Test
+//	public final void testDeletePointAtMiddleOfCurve() 
+//	{
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	/**
+//	 * Test method for {@link apcs.economics.ProducerCurve#deletePointAtEndOfCurve(apcs.economics.Point)}.
+//	 */
+//	@Test
+//	public final void testDeletePointAtEndOfCurve() 
+//	{
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	/**
+//	 * Test method for {@link apcs.economics.ProducerCurve#setPoint(apcs.economics.Point, int)}.
+//	 */
+//	@Test
+//	public final void testSetPoint() 
+//	{
+//		Point[] pointArray = new Point[10];
+//		Point testPoint = new Point(1, 1.0);
+//		ProducerCurve testProducerCurve = new ProducerCurve(pointArray);
+//		testProducerCurve.setPoint(testPoint, 7);
+//		
+//		//assertTrue(testProducerCurve.getPoint(7).equals(testPoint));
+//		assertEquals(testPoint, testProducerCurve.getPointInCurve(7));
+//		
+//	}
+//
+//	/**
+//	 * Test method for {@link apcs.economics.ProducerCurve#isEmpty()}.
+//	 */
+//	@Test
+//	public final void testIsEmpty() 
+//	{
+//		Point[] curvePoints = new Point[10];
+//		ProducerCurve testProducerCurve = new ProducerCurve(curvePoints);
+//		assertTrue("Curve is Empty.", testProducerCurve.isEmpty());
+//		
+//		Point testPoint = new Point(1, 1.0);
+//		testProducerCurve.addPointToCurve(testPoint, 5);
+//		assertFalse("Curve is not empty.", testProducerCurve.isEmpty());
+//	}
+//
+//	public final void testToString()
+//	{
+//		Point[] curvePoints = new Point[10];
+//		ProducerCurve testProducerCurve = new ProducerCurve(curvePoints);
+//		
+//		
+//		assertTrue(testProducerCurve.toString());
+//	}
 }
