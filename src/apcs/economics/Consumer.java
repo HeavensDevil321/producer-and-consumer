b7028df7d4c676be10af496bbb5b5de2cf976318
ggrivera <ggrivera@310A26103.district.local>
2015-09-30 10:13:00 -0500

    worked on the producer and consumer. Mr.M said that it could be in one class or 2 but dont know for sure how it will turn out.

--- src/apcs/economics/Consumer.java
+++ src/apcs/economics/Consumer.java
@@ -9,5 +9,31 @@ package apcs.economics;
  */
 public class Consumer 
 {
+	private ConsumerCurve consumer;
 	
+	public Consumer()
+	{
+		consumer = new ConsumerCurve();
+	}
+	
+	public Consumer(int np, double m, double b, int dx)
+	{
+		consumer = new ConsumerCurve(np, m, b, dx);
+	}
+	
+	public Point respondToBid(Point point)
+	{
+		point = null;
+				
+		if (consumer.contain(point) == true)
+		{
+			return point;
+		}
+		else
+		{
+			
+		}
+		
+		return point;
+	}
 }
--- src/apcs/economics/Producer.java
+++ src/apcs/economics/Producer.java
@@ -9,5 +9,31 @@ package apcs.economics;
  */
 public class Producer 
 {
-
+	private ProducerCurve producer;
+	
+	public Producer()
+	{
+		producer = new ProducerCurve();
+	}
+	
+	public Producer(int np, double m, double b, int dx)
+	{
+		producer = new ProducerCurve(np, m, b, dx);
+	}
+	
+	public Point respondToBid(Point point)
+	{
+		point = null;
+				
+		if (producer.contain(point) == true)
+		{
+			return point;
+		}
+		else
+		{
+			
+		}
+		
+		return point;
+	}
 }
