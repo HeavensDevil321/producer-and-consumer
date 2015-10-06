1fcc7ad443df26c40d1ccffbbfff386ba6d6546f
Gabriel Rivera <elggrivera@gmail.com>
2015-10-05 20:48:12 -0500

    major fix need to be made

--- src/apcs/economics/Consumer.java
+++ src/apcs/economics/Consumer.java
@@ -4,29 +4,43 @@
 package apcs.economics;
 
 /**
- * @author ggrivera
- *
+ * @author german rivera
+ * This is the consumer class in which is the buyer of the market
+ * its respond to bids are below the consumer curve or on the curve.
  */
 public class Consumer 
 {
 	private ConsumerCurve consumer;
 	
+	/**
+	 * Instantiate the consumer
+	 */
 	public Consumer()
 	{
 		consumer = new ConsumerCurve();
 	}
 	
+	/**
+	 * constructor that deals with the slope
+	 * @param np
+	 * @param m
+	 * @param b
+	 * @param dx
+	 */
 	public Consumer(int np, double m, double b, int dx)
 	{
 		consumer = new ConsumerCurve(np, m, b, dx);
 	}
 	
+	/**
+	 * this is the response of the consumer to the bid
+	 * @param point
+	 * @return
+	 */
 	public Point respondToBid(Point point)
 	{
 		Point p1 = null;
 		
-		int temp = 0;
-						
 		if (consumer.contain(point) == true)
 		{
 			p1 = point;
--- src/apcs/economics/Producer.java
+++ src/apcs/economics/Producer.java
@@ -4,55 +4,77 @@
 package apcs.economics;
 
 /**
- * @author ggrivera
- *
+ * @author german rivera
+ * Producer is the seller of the market
  */
 public class Producer 
 {
-	private ProducerCurve curvePoint;
+	private ProducerCurve producer;
 	
+	/**
+	 * Instantiate the producer
+	 */
 	public Producer()
 	{
-		curvePoint = new ProducerCurve();
+		producer = new ProducerCurve();
 	}
 	
+	/**
+	 * constructor that deals with the slope
+	 * @param np
+	 * @param m
+	 * @param b
+	 * @param dx
+	 */
 	public Producer(int np, double m, double b, int dx)
 	{
-		curvePoint = new ProducerCurve(np, m, b, dx);
+		producer = new ProducerCurve(np, m, b, dx);
 	}
 	
+	/**
+	 * this is the response of the producer to the bid
+	 * @param point
+	 * @return
+	 */
 	public Point respondToBid(Point point)
 	{
 		Point p1 = null;
 		
-		int temp = 0;
+		
 						
-		if (curvePoint.contain(point) == true)
+		if (producer.contain(point) == true)
 		{
 			p1 = point;
 		}
 		else
 		{
-			for(int i = 0; i < curvePoint.getList().size(); i++)
+			for(int i = 0; i < producer.getList().size(); i++)
 			{
 				
-				if(curvePoint.getList().get(i).getQuantity() == point.getQuantity())
+				if(producer.getList().get(i).getQuantity() == point.getQuantity())
 				{
-					if (curvePoint.getList().get(i).getPrice() < point.getPrice())
+					if (producer.getList().get(i).getPrice() < point.getPrice())
 					{
 						p1 = point;
 					}
 					else
 					{
-						p1 = curvePoint.getList().get(i);
+						p1 = producer.getList().get(i);
 					}
-					if (curvePoint.getList().get(i).getQuantity() > point.getQuantity())
+					if (producer.getList().get(i).getPrice() > point.getPrice())
 					{
 						p1 = point;
 					}
 				}
 				
-				
+				if (producer.getList().get(i).getQuantity() < point.getQuantity())
+				{
+					p1 = point;
+				}
+				if (producer.getList().get(i).getQuantity() > point.getQuantity())
+				{
+					p1 = point;
+				}
 			}
 		}		
 		return point;
